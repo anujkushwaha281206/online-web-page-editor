f4cbed110d28e07bdc24fbf71466c0b78c5c8ad5
anuj kushwaha <84984247+anujkushwaha281206@users.noreply.github.com>
2025-05-30 20:28:22 +0530

    Delete web editor directory

--- web editor/script.js
+++
@@ -1,7 +0,0 @@
-function start(){
-    let html = document.getElementById("html").value;
-    let css = "<style>" + document.getElementById("css").value + "</style>";
-    let js =document.getElementById("js");
-    document.getElementById("output").contentDocument.body.innerHTML = html + css;
-    document.getElementById("output").contentWindow.eval(js.value);
-}
